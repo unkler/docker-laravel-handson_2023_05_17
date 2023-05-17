273faaac240ff64fd759a4efe4f2c27786a28db9
unkler <unkler0830@gmail.com>
2023-05-17 16:09:45 +0900

    laravel breezeをインストール #1 対応

--- src/resources/js/components/TheHeader.vue
+++ src/resources/js/components/TheHeader.vue
@@ -20,12 +20,9 @@
           <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
         </li>
         <li>
-          <a href="#" class="flex items-center mr-4 hover:text-blue-100">
-            <span class="inline-flex mr-1">
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
-            </span>
-            Logout
-          </a>
+          <p @click="logout" class="flex items-center mr-4 text-xs cursor-pointer">
+            ログアウト
+          </p>
         </li>
       </ul>
     </div>
@@ -34,9 +31,36 @@
 
 <script>
 import ToggleDarkMode from './ToggleDarkMode.vue'
+import { errorMessage } from '../constants/message'
+
 export default {
   components: {
     ToggleDarkMode
-  }
+  },
+  data() {
+    return {
+      isLoggingOut: false,
+    }
+  },
+  methods: {
+    logout() {
+      // 既にログアウト処理が実行中の場合は、二重送信を防ぐために処理を終了
+      if (this.isLoggingOut) return
+
+      this.isLoggingOut = true
+
+      axios.post('/logout')
+        .then(res => {
+          location.href = '/login'
+        })
+        .catch(error => {
+          console.error(error)
+          this.$toasted.error(errorMessage.systemError)
+        })
+        .finally(() => {
+        this.isLoggingOut = false
+        })
+    }
+  },
 }
 </script>
