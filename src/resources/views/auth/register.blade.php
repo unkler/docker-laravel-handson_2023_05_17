8f96170c97623e4c0b9499de2e99d22b706cd60b
unkler <unkler0830@gmail.com>
2023-05-18 16:43:01 +0900

    日本語対応の漏れを修正する #9 ログイン画面などのデザインを変更 #4の不要なコメントアウトを削除

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -8,15 +8,11 @@
 
             <!-- Name -->
             <div>
-                {{-- <x-label for="name" :value="__('Name')" /> --}}
-
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" placeholder="{{ __('Name') }}" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                {{-- <x-label for="email" :value="__('Email')" /> --}}
-
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="{{ __('Email') }}" required />
             </div>
 
