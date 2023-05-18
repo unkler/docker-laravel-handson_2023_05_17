6baa065906a6d1049ead06d9a5b94215752bff9d
unkler <unkler0830@gmail.com>
2023-05-18 14:57:59 +0900

    ログイン、ユーザー登録画面のログを変更

--- src/resources/views/components/auth-card.blade.php
+++ src/resources/views/components/auth-card.blade.php
@@ -1,9 +1,12 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-blue-800">
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+      <div class="flex justify-center items-center mb-5">
+        <div class="text-center">
+          <img src="/storage/images/logo.jpg" width="50" class="m-auto">
+          <p class="text-gray-500 inline-block align-middle">Devide Up</p>
+        </div>
+      </div>
+
         {{ $slot }}
     </div>
 </div>
