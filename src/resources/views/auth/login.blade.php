f99445d24270d0d6217a20bbd2c803cebfb345e3
unkler <unkler0830@gmail.com>
2023-05-19 01:32:54 +0900

    ユーザー登録画面へのリンク #12 対応

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -34,7 +34,10 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Register As A New User') }}
+                </a>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
